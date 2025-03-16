a3c6b41ff1dd1267f02096ad568e4a830f76c695
AgushPy <agusvald18@gmail.com>
2025-03-16 16:00:11 -0300

    configs in vercel and dependency push

--- app/index.ts
+++ app/index.ts
@@ -25,11 +25,11 @@ app.get('/', (req,res) => {
 })
 
 
-if ( process.env.NODE_ENV !== 'production' ) {
+// if ( process.env.NODE_ENV !== 'production' ) {
   const PORT = process.env.PORT || 3000;
   app.listen( PORT, () => {
     console.log( `Servidor corriendo en el puerto ${ PORT }` );
   } );
-}
+// }
 
-export default app;
+// export default app;
