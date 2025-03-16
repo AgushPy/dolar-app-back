d459527ad4a025c1920ad333f786fa781c7783cb
AgushPy <agusvald18@gmail.com>
2025-03-16 17:23:44 -0300

    out empty objects

--- app/controller/scraperController.ts
+++ app/controller/scraperController.ts
@@ -11,7 +11,18 @@ export const getDolarBlue = async ( req: Request, res: Response ) => {
 
       console.log('El resultado de redis fue',getResultDolars)
       if ( getResultDolars ) {
-        res.json( JSON.parse( getResultDolars ) );
+
+        const parsedResult = JSON.parse(getResultDolars);
+
+        if (Array.isArray(parsedResult) && parsedResult.every(item => Object.keys(item).length === 0)) {
+          console.log('Redis devolvió objetos vacíos, realizando scraping');
+          // Realiza el scraping si los objetos están vacíos
+          const dolarValue: Dolar[] = await getDolarBlueValues(req, res);
+          res.json(dolarValue);
+          return;
+        }
+        
+        res.json( JSON.parse( parsedResult ) );
         return;
       }
 
